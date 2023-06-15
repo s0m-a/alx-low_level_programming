397a7a6496f0dd4bac7bc82551f43ae37ed353b6
ogbenna nmesoma anita <ogbennaanita@gmail.com>
2023-06-15 16:13:20 -0700

    5-printf.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
-#!/bin/bash
 #include<stdio.h>
+/**
+ *  * this is a comment
+ *   *
+ *    * this is a comment 
+ *     */
 int main(void)
 {
-	        puts("\"Programming is like building a multilingual puzzle");
-       return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
