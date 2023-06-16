f13b236818a0a4e249b65dff2033f2d8e723f7d8
ogbenna nmesoma anita <ogbennaanita@gmail.com>
2023-06-16 03:58:19 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
 #include<stdio.h>
 /**
-*main - start point
-*Description: 'the program's description' 
-*Return: Returns 0
-*/
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
-{puts("\"Programming is like building a multilingual puzzle");
+{
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
+
