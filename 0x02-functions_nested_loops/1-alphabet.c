ec3baaf96c78c7d4918b9e5ed27101316a837d1d
ogbenna nmesoma anita <ogbennaanita@gmail.com>
2023-06-20 13:13:11 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+*main - code starts here
+*description : print all alphabet in lowercase
+Return : Returns  0 sucess
+*/
+void print_alphabet(void)
+{
+char letter;
+for (letter = 'a'; letter <= 'z'; letter++)
+_putchar(letter);
+_putchar('\n');
+}
