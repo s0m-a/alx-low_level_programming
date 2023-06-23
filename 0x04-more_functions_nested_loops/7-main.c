79f800a68913a7701e1e3714cb73fd35d887a383
ogbenna nmesoma anita <ogbennaanita@gmail.com>
2023-06-22 19:47:15 -0700

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-main.c
+++ 0x04-more_functions_nested_loops/7-main.c
@@ -2,11 +2,14 @@
 
 /**
  * main - check the code
+ *
  * Return: Always 0.
  */
 int main(void)
 {
-print_numbers();
+print_diagonal(0);
+print_diagonal(2);
+print_diagonal(10);
+print_diagonal(-4);
 return (0);
 }
-
