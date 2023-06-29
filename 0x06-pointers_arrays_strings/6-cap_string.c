7b7a0d59071d99ff86c8c6165c73a38823816df6
ogbenna nmesoma anita <ogbennaanita@gmail.com>
2023-06-29 06:22:57 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -3,6 +3,7 @@
 /**
  * cap_string - Capitalizes all words of a string.
  * @str: The string to be capitalized.
+ *
  * Return: A pointer to the changed string.
  */
 char *cap_string(char *str)
@@ -29,9 +30,7 @@ char *cap_string(char *str)
 		    str[index - 1] == '}' ||
 		    index == 0)
 			str[index] -= 32;
-
-		index++;
+	index++;
 	}
-
-}	return (str);
-
+	return (str);
+}
