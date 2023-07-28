9ca3b60e48dfd32b07ab0e6ba093a74f0da6d82d
ogbenna nmesoma anita <ogbennaanita@gmail.com>
2023-07-28 14:56:26 -0700

    0-print_list.c

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * print_list - prints elements of the list
+ * @h:a pointer
+ * Return:the number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+size_t count = 0;
+
+while (h)
+{
+if (!h->str)
+printf("[0] (nil) \n");
+else
+printf("[%u] %s\n", h->len, h->str);
+h = h->next;
+count++;
+}
+return (count);
+}
