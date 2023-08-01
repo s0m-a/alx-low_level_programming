5adb98d87f01f7f10741e546206a1a91cc6cca2f
ogbenna nmesoma anita <ogbennaanita@gmail.com>
2023-08-01 15:51:36 -0700

    updates

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,9 +1,9 @@
+#include "lists.h"
 /**
  * pop_listint - function that deletes the head node of a listint_t linked list
  * @head: pointer to the first node
  * Return: returns the head node’s data (n).
  */
-#include "lists.h"
 int pop_listint(listint_t **head)
 {
 if (*head == NULL)
--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,21 +1,18 @@
-
+#include "lists.h"
 /**
- * get_nodeint_at_index -function that returns the nth node of a the list
- * @index: index of the node
- * @head: first node of the list
- * Return: returns the nth node of a listint_t linked list
+ * get_nodeint_at_index - function that returns the nth node of a list
+ * @index:node being return
+* @head: first node
+ * Return: returns pointer to the node or a NULL
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-unsigned int pos = 0;
-while (head != NULL && pos < index)
+unsigned int count = 0;
+listint_t *ptr = head;
+while (ptr && count < index)
 {
-head = head->next;
-pos++;
+ptr = ptr->next;
+count++;
 }
-if (head == NULL)
-{
-return (NULL);
-}
-return (head);
+return (ptr ? ptr : NULL);
 }
