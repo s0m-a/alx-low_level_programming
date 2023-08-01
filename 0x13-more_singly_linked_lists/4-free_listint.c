b9d893d089b1cdd4f4a2ca199b3c0760be268486
ogbenna nmesoma anita <ogbennaanita@gmail.com>
2023-08-01 08:27:04 -0700

    updates

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,16 @@
+/**
+ * free_listint - function that frees a linked list
+ * @head: list to be freed
+ */
+#include "lists.h"
+
+void free_listint(listint_t *head)
+{
+listint_t *temp;
+while (head)
+{
+temp = head->next;
+free(head);
+head = temp;
+}
+}
