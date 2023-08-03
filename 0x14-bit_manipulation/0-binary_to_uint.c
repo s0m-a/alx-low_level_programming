c9bb87b1ae3f21017546bd6ce20a1eebe581a8e4
ogbenna nmesoma anita <ogbennaanita@gmail.com>
2023-08-03 12:34:36 -0700

    updates

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,11 +10,12 @@ unsigned int binary_to_uint(const char *b)
 {
 int i;
 unsigned int decimal = 0;
+b = NULL;
 if (!b)
 return (0);
 for (i = 0; b[i]; i++)
 {
-if (b[i] < '0' || b[i] > '1')			
+if (b[i] < '0' || b[i] > '1')
 return (0);
 decimal = 2 * decimal + (b[i] - '0');
 }
