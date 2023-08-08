953a1ea4e88573c56ddecf2aea1009426bb55909
ogbenna nmesoma anita <ogbennaanita@gmail.com>
2023-08-08 07:15:20 -0700

    updates

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,21 +1,22 @@
 #include "main.h"
 /**
  * flip_bits - function that returns the number of bits
- *you would need to flip to get from one number to another.
- * @n: first number
- * @m: second number
- * Return: returns the number of bits
+ * you would need to flip to get from one number to another.
+ * @n: first integer
+ * @m: second integer
+ * Return:the number of bits been changed
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-unsigned long int result;
-unsigned long int XOR = n ^ m;
+int i = 0;
 int count = 0;
-for (int i = 63; i >= 0; i--)
+unsigned long int exclusive = n ^ m;
+unsigned long int pre;
+for (i = 63; i >= 0; i--)
 {
-result = XOR >> i;
-if (result & 1)
-count++;
+pre = exclusive >> i;
+if (pre & 1)
+count += 1;
 }
 return (count);
 }
