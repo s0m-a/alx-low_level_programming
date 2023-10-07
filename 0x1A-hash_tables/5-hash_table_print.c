e27ffaaca5cc74c36a76d936ece2e13f5c5e8a6c
ogbenna nmesoma anita <ogbennaanita@gmail.com>
2023-10-06 19:41:17 -0700

    updates

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -2,29 +2,35 @@
 
 /**
  * hash_table_print - function that prints a hash table
- * @ht: the hash table
- * Return: Returns nothing
+ * @ht:hash table
+ * Return:Returns nothing
  */
-
 void hash_table_print(const hash_table_t *ht)
 {
-    char flag = 0;
+unsigned char c_flag = 0;
+	hash_node_t *node;
 	unsigned long int x;
-	hash_node_t *temp;
 
-	if (ht == NULL || ht->array == NULL)
+	if (ht == NULL)
 		return;
+
 	printf("{");
 	for (x = 0; x < ht->size; x++)
 	{
-		temp = ht->array[x];
-		while (temp != NULL)
+		if (ht->array[x] != NULL)
 		{
-			if (flag == 1)
+			if (c_flag == 1)
 				printf(", ");
-			printf("'%s': '%s'", temp->key, temp->value);
-			flag = 1;
-			temp = temp->next;
+
+			node = ht->array[x];
+			while (node != NULL)
+			{
+				printf("'%s': '%s'", node->key, node->value);
+				node = node->next;
+				if (node != NULL)
+					printf(", ");
+			}
+			c_flag = 1;
 		}
 	}
 	printf("}\n");
