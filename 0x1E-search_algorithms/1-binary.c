981dd26e4368082b68663338670731d1b7d8a9d0
ogbenna nmesoma anita <ogbennaanita@gmail.com>
2024-01-17 14:19:01 -0800

    updates

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,33 +1,51 @@
 #include "search_algos.h"
+/**
+ * print - prints arr
+ * @left: first index to print
+ * @right: last index to print
+ * @array: array to print
+ *
+ * Return: void
+ */
+void print(size_t left, size_t right, int *array)
+{
+	size_t i;
+
+	printf("Searching in array: ");
+	for (i = left; i <= right - 1; i++)
+		printf("%d, ", array[i]);
+	printf("%d\n", array[right]);
+}
 
 /**
- * binary_search - binary search function
- * @array:first element of array to seach
- * @size: num of elements in array
- * @value: value we are looking for
- * Return: index containing `value`, or -1 if `value` not found
+ * binary_search - function that searches for a value
+ * @array: Ptr to first element of the array
+ * @size: Number of elements in array
+ * @value: Value to search for
+ *
+ * Return: The index where value is located or -1 on fail
  */
-
 int binary_search(int *array, size_t size, int value)
 {
-	int i, left, right;
+	size_t left = 0;
+	size_t right = size - 1;
+	size_t mid;
 
-	if (!array)
+	if (array == NULL)
 		return (-1);
-	for (left = 0, right = (int)size - 1; right >= left;)
-	{
-		printf("Searching in array: ");
-		for (i = left; i < right; i++)
-			printf("%d, ", array[i]);
-		printf("%d\n", array[i]);
 
-		i = left + (right - left) / 2;
-		if (array[i] == value)
-			return (i);
-		if (array[i] > value)
-			right = i - 1;
-		else
-			left = i + 1;
+	while (left <= right)
+	{
+		print(left, right, array);
+
+		mid = left + (right - left) / 2;
+
+		if (value == array[mid])
+			return (mid);
+		else if (value > array[mid])
+			left = mid + 1;
+		else if (value < array[mid])
+			right = mid - 1;
 	}
 	return (-1);
 }
--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -2,32 +2,43 @@
 #include <math.h>
 
 /**
- * jump_search -  jump search algorithm function
- * @array: first elem of array
- * @size: number of elem in array
- * @value: value to search
- * Return: first index containing `value`, or -1 if `value` not found
+ * jump_search - function that searches for a value
+ * @array: Ptr to the first element
+ * @size: Number of elements in array
+ * @value: Value to search for
+ *
+ * Return: The first index where value is located, or -1 on fail
  */
 
 int jump_search(int *array, size_t size, int value)
 {
-	size_t i = 0, n, jump;
+	int i = 0;
+	int jump = (int)sqrt(size);
+	int low, high;
 
-	if (!array || size == 0)
+	if (array == NULL)
 		return (-1);
 
-	for (n = sqrt(size); i < size; i += n)
+	while (i < (int)size && array[i] < value)
 	{
-		if (array[i] >= value)
-			break;
-		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+		printf("Value checked array[%d] = [%d]\n", i, array[i]);
+		i = i + jump;
 	}
-	printf("Value found between indexes [%lu] and [%lu]\n", i - n, i);
-	for (jump = i - n; jump < size && jump <= i; jump++)
+
+	low = i - jump;
+	high = i;
+
+	printf("Value found between indexes [%d] and [%d]\n", low, high);
+
+	for (i = low; i <= high; i++)
 	{
-		printf("Value checked array[%lu] = [%d]\n", jump, array[jump]);
-		if (array[jump] == value)
-			return (jump);
+		if (i < (int)size)
+		{
+			printf("Value checked array[%d] = [%d]\n", i, array[i]);
+			if (array[i] == value)
+				return (i);
+		}
 	}
+
 	return (-1);
 }
